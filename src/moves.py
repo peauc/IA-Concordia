db44af29fc4ebb4b327234d935306b01e774318d
tonell_m <marceau.tonelli@epitech.eu>
2018-10-09 12:21:29 -0400

    Started a BFS algorithm

--- src/moves.py
+++ src/moves.py
@@ -1,67 +1,80 @@
-WIDTH = 4
-HEIGHT = 3
-SIZE = WIDTH * HEIGHT
+"""
+
+This file will contain the different functions and constants to interact with the "board".
+
+"""
+
+# Import statements
+
+from node import Node
+
+# =========
+# CONSTANTS
+# =========
+
+# Board dimensions
+WIDTH: int = 4
+HEIGHT: int = 3
+SIZE: int = WIDTH * HEIGHT
+
+# Moves index shift in a single dimension array
+MOVE_UP_INDEX_SHIFT: int = -WIDTH
+MOVE_DOWN_INDEX_SHIFT: int = WIDTH
+MOVE_LEFT_INDEX_SHIFT: int = -1
+MOVE_RIGHT_INDEX_SHIFT: int = 1
+MOVE_UP_LEFT_INDEX_SHIFT: int = MOVE_UP_INDEX_SHIFT - 1
+MOVE_UP_RIGHT_INDEX_SHIFT: int = MOVE_UP_INDEX_SHIFT + 1
+MOVE_DOWN_LEFT_INDEX_SHIFT: int = MOVE_DOWN_INDEX_SHIFT - 1
+MOVE_DOWN_RIGHT_INDEX_SHIFT: int = MOVE_DOWN_INDEX_SHIFT + 1
+
+# Array containing the possible moves in the order of preference (clock-wise starting from move_up)
+MOVES: list = [
+    MOVE_UP_INDEX_SHIFT,
+    MOVE_UP_RIGHT_INDEX_SHIFT,
+    MOVE_RIGHT_INDEX_SHIFT,
+    MOVE_DOWN_RIGHT_INDEX_SHIFT,
+    MOVE_DOWN_INDEX_SHIFT,
+    MOVE_DOWN_LEFT_INDEX_SHIFT,
+    MOVE_LEFT_INDEX_SHIFT,
+    MOVE_UP_LEFT_INDEX_SHIFT
+]
 
 
-def move_up(map_array, pos):
-    if pos - WIDTH < 0:
-        return map_array
+# =========
+# FUNCTIONS
+# =========
 
-    map_array[pos - WIDTH], map_array[pos] = map_array[pos], map_array[pos - WIDTH]
-    return map_array
+def get_children_nodes(node: Node, closed_list: list, open_list: list) -> list:
+    """Returns a list of the children nodes of the given node, in order of preference."""
 
+    possible_moves = []
+    # closed_states_list = map(lambda closed_list_node: closed_list_node.state_map, closed_list)
 
-def move_down(map_array, pos):
-    if pos + WIDTH > SIZE - 1:
-        return map_array
+    for move in MOVES:
+        child: Node = create_child(node, move)
 
-    map_array[pos + WIDTH], map_array[pos] = map_array[pos], map_array[pos + WIDTH]
-    return map_array
+        if child in closed_list and child not in open_list and child is not None:
+            possible_moves.append(child)
 
-
-def move_left(map_array, pos):
-    if pos - 1 < 0:
-        return map_array
-
-    map_array[pos - 1], map_array[pos] = map_array[pos], map_array[pos - 1]
-    return map_array
+    print("possible moves:", list(map(lambda x: x.__str__(), possible_moves)), "\n")
+    return possible_moves
 
 
-def move_right(map_array, pos):
-    if pos + 1 > SIZE - 1:
-        return map_array
+def can_make_move(position: int, index_shift: int) -> bool:
+    """True if the targeted index is in the array bounds."""
 
-    map_array[pos + 1], map_array[pos] = map_array[pos], map_array[pos + 1]
-    return map_array
+    return SIZE > position + index_shift > 0
 
 
-def move_up_left(map_array, pos):
-    if pos - WIDTH - 1 < 0:
-        return map_array
+def create_child(state: Node, move: int) -> Node:
+    """Returns a new node for the given move at the given state"""
 
-    map_array[pos - WIDTH - 1], map_array[pos] = map_array[pos], map_array[pos - WIDTH - 1]
-    return map_array
+    if not can_make_move(state.empty_tile_index, move):
+        return None
 
+    next_index = state.empty_tile_index + move
+    state_map = state.state_map
+    state_map[state.empty_tile_index], state_map[next_index] = state_map[next_index], state_map[state.empty_tile_index]
 
-def move_down_left(map_array, pos):
-    if pos + WIDTH - 1 > SIZE - 1:
-        return map_array
-
-    map_array[pos + WIDTH - 1], map_array[pos] = map_array[pos], map_array[pos + WIDTH - 1]
-    return map_array
-
-
-def move_up_right(map_array, pos):
-    if pos - WIDTH + 1 < 0:
-        return map_array
-
-    map_array[pos - WIDTH + 1], map_array[pos] = map_array[pos], map_array[pos - WIDTH + 1]
-    return map_array
-
-
-def move_down_right(map_array, pos):
-    if pos + WIDTH + 1 > SIZE - 1:
-        return map_array
-
-    map_array[pos + WIDTH + 1], map_array[pos] = map_array[pos], map_array[pos + WIDTH + 1]
-    return map_array
+    print("position: ", next_index)
+    return Node(state_map[:], next_index, move, state)
