c0ec3e09035cad6aac59484b475e351b6bc15526
PEAU Clement <clement.peau@epitech.eu>
2018-10-09 13:28:43 -0400

    /dfs:
    Merged with master

--- src/Algorithm/BreadthFirst.py
+++ src/Algorithm/BreadthFirst.py
@@ -7,18 +7,18 @@ from src.node import Node
 class BreathFirst(IAlgorythm):
 
     def compute(self, board, heuristics) -> list:
-        current_node = Node(board, board.index(0), None, None)
-        open_list = []
-        closed_list = []
-        while not is_goal(current_node):
-            closed_list.append(current_node)
-            next_moves = get_children_nodes(current_node, closed_list, open_list)
-            open_list = next_moves + open_list
-            current_node = open_list.pop(0)
-            print("open_list: ", list(map(lambda x: x.__str__(), open_list)), "\n")
-            print("closed_list: ", list(map(lambda x: x.__str__(), closed_list)), "\n")
-            print("current_node: ", current_node, "\n")
-            input()
+
+        self._closed_list.append(self._current_node)
+        next_moves = get_children_nodes(self._current_node, self._closed_list, self._open_list)
+        open_list = next_moves + self._open_list
+        current_node = open_list.pop(0)
+        self.__str__()
+        input()
+
+    def __str__(self):
+        print("open_list: ", list(map(lambda x: x.__str__(), self._open_list)), "\n")
+        print("closed_list: ", list(map(lambda x: x.__str__(), self._closed_list)), "\n")
+        print("current_node: ", self._current_node, "\n")
 
     def __init__(self, initial_board_state: list):
         super(BreathFirst, self).__init__(initial_board_state)
