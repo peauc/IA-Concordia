8c472df3f2b5e69ed77f2ddb19bfc422e2a0a7de
PEAU Clement <clement.peau@epitech.eu>
2018-10-09 12:01:13 -0400

    Starting to work on the actual algorithms

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import src.core as core
 from src.Algorithm.BreadthFirst import BreathFirst
+from src.Algorithm.IAlgorythm import IAlgorythm
 
 
 def initBF():
     algo = BreathFirst()
     if not algo.is_resolved():
         print("initiated correctly")
-
+    core.loop(algo, None)
 
 def main():
     initBF()
